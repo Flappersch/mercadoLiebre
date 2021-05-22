93ceb060a7b8d6d977280c0deddb99a8a9c00bd7
Flappersch <schlattef@yahoo.com.ar>
2021-05-22 17:10:28 -0300

    Subiendo TP para correccion en DH

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const app = express ();
+
+const path = require ('path');
+
+/*Configuraciones*/
+
+app.use(express.static('public'));
+
+// alternativa ---> app.use (express.static(path.resolve(__dirname, 'public'))); <------
+
+/*Rutas*/
+
+app.get('/', (req, res) => {
+    res.sendFile (path.join(__dirname,'/views/home.html'));
+}),
+
+app.get('/register', (req, res) => {
+    res.sendFile (path.join(__dirname,'/views/register.html'));
+}),
+
+app.get('/login', (req, res) => {
+    res.sendFile (path.join(__dirname,'/views/login.html'));
+}),
+
+/* Server */
+
+app.listen('3000', () => console.log ('Estamos corriente en el puerto 3000'));
+
